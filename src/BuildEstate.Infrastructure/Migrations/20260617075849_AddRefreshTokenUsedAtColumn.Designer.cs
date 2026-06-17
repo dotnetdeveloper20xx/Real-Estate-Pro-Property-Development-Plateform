7957a81663a4bea7dba62ce26c569b05a6e202cf
Dotnetdeveloper <dotnetdeveloper20xx@hotmail.com>
2026-06-17 09:00:54 +0100

    fix: add missing UsedAt column migration for RefreshToken table

--- src/BuildEstate.Infrastructure/Migrations/20260617075849_AddRefreshTokenUsedAtColumn.Designer.cs
+++ src/BuildEstate.Infrastructure/Migrations/20260617075849_AddRefreshTokenUsedAtColumn.Designer.cs
@@ -4,6 +4,7 @@ using BuildEstate.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BuildEstate.Infrastructure.Migrations
 {
     [DbContext(typeof(BuildEstateDbContext))]
-    partial class BuildEstateDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260617075849_AddRefreshTokenUsedAtColumn")]
+    partial class AddRefreshTokenUsedAtColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/BuildEstate.Infrastructure/Migrations/20260617075849_AddRefreshTokenUsedAtColumn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuildEstate.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenUsedAtColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UsedAt",
+                table: "RefreshTokens",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UsedAt",
+                table: "RefreshTokens");
+        }
+    }
+}
